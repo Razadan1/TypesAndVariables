b7d8d3215f0ccdf326761258a0ccacb47e6c3033
Raza <danmoleomomayowa@gmail.com>
2025-02-12 17:41:53 +0100

    Task 10, building a heart on the console

--- Program.cs
+++ Program.cs
@@ -1,52 +1,62 @@
-﻿using TypesAndVariables;
+﻿// using TypesAndVariables;
+// using TypesAndVariables.obj;
+// // See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Types and Variables");
+// Console.WriteLine("Hello, World!");
+// Console.WriteLine("Here we will be solving issues on Types and variables");
+
+
+// Console.WriteLine("\n\n\nExercise 1");
+// Exercise1 exercise1 = new Exercise1();
+// exercise1.solveExcersice1();
+
+
+// Console.WriteLine("\n\n\nExercise 2");
+// Exercise2 exercise2 = new Exercise2();
+// exercise2.solveExercise2();
+
+
+// Console.WriteLine("\n\n\nExercise 3");
+// Exercise3 exercise3 = new Exercise3();
+// exercise3.solveExercise3();
+
+// Console.WriteLine("\n\n\nExercise 4");
+// Exercise4 exercise4 = new Exercise4();
+// exercise4.solveExercise4();
+
+// Console.WriteLine("\n\n\nExercise 5");
+// Exercise5 exercise5 = new Exercise5();
+// exercise5.solveExercise5();
+
+// Console.WriteLine("\n\n\tExercise 6");
+// Exercise6 exercise6 = new Exercise6();
+// exercise6.solveExercise6();
+
+// Console.WriteLine("\n\n\tExcercise 7");
+// Excercise7 exercise7 = new Excercise7();
+// exercise7.solveExercise7();
+
+
+// Console.WriteLine("\n\n\tExcercise 8");
+// Exercise8 exercise8 = new Exercise8();
+// exercise8.solveExercise8();
+
+
+// Console.WriteLine("\n\n\tExcercise 9");
+// Exercise9 exercise9 = new Exercise9();
+// exercise9.solveExercise9();
+
 using TypesAndVariables.obj;
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Types and Variables");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Here we will be solving issues on Types and variables");
 
-
-Console.WriteLine("\n\n\nExercise 1");
-Exercise1 exercise1 = new Exercise1();
-exercise1.solveExcersice1();
-
-
-Console.WriteLine("\n\n\nExercise 2");
-Exercise2 exercise2 = new Exercise2();
-exercise2.solveExercise2();
-
-
-Console.WriteLine("\n\n\nExercise 3");
-Exercise3 exercise3 = new Exercise3();
-exercise3.solveExercise3();
-
-Console.WriteLine("\n\n\nExercise 4");
-Exercise4 exercise4 = new Exercise4();
-exercise4.solveExercise4();
-
-Console.WriteLine("\n\n\nExercise 5");
-Exercise5 exercise5 = new Exercise5();
-exercise5.solveExercise5();
-
-Console.WriteLine("\n\n\tExercise 6");
-Exercise6 exercise6 = new Exercise6();
-exercise6.solveExercise6();
-
-Console.WriteLine("\n\n\tExcercise 7");
-Excercise7 exercise7 = new Excercise7();
-exercise7.solveExercise7();
-
-
- 
-
-// // 10. Write a program to print a figure in the shape of a heart by the sign "o". 
-// // 11. Write a program that prints on the console isosceles triangle which 
-// // sides consist of the copyright character "©". 
-// // 12. A company dealing with marketing wants to keep a data record of its 
-// // employees. Each record should have the following characteristic – first 
-// // name, last name, age, gender (‘m’ or ‘f’) and unique employee number 
-// // (27560000 to 27569999). Declare appropriate variables needed to 
-// // maintain the information for an employee by using the appropriate data 
-// // types and attribute names. 
-// // 13. Declare two variables of type int. Assign to them values 5 and 10 
-// // respectively. Exchange (swap) their values and print them.
+Excercise10 excercise10 = new Excercise10();
+excercise10.solveExcersice10();
+// // // 11. Write a program that prints on the console isosceles triangle which 
+// // // sides consist of the copyright character "©". 
+// // // 12. A company dealing with marketing wants to keep a data record of its 
+// // // employees. Each record should have the following characteristic – first 
+// // // name, last name, age, gender (‘m’ or ‘f’) and unique employee number 
+// // // (27560000 to 27569999). Declare appropriate variables needed to 
+// // // maintain the information for an employee by using the appropriate data 
+// // // types and attribute names. 
+// // // 13. Declare two variables of type int. Assign to them values 5 and 10 
+// // // respectively. Exchange (swap) their values and print them.
